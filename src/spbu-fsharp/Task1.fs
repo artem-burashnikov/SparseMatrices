eae0190bebbfde6d6f18f1e06a01526b327b80e3
artem-burashnikov <artem.burashnikov@gmail.com>
2022-10-02 00:47:40 +0300

    Changed file structure for Task1

---
+++ src/spbu-fsharp/Task1.fs
@@ -0,0 +1,104 @@
+module HomeWork1
+
+// Homework 1 - Task 1.
+// Power function.
+// This function takes two numbers (base and exponent)
+// and iteratively calculates a power of base to the exponent.
+let pow (arg: float) (exp: int) : float =
+
+    // This operation is not defined
+    if arg = 0.0 && exp = 0 then
+        failwith "Undefined"
+
+    // Accumulate the product through a loop.
+    let mutable result = 1.0
+
+    for i = 1 to abs exp do
+        result <- result * arg
+
+    // For a positive exponent return the value as is.
+    // For a negative exponent inverse the number.
+    if exp > 0 then result else 1.0 / result
+
+
+
+// Homework 1 - Task 2.
+// Quick power function.
+// Ths function takes two numbers (base and exponent)
+// and recursively calculates a power of base to the exponent.
+let rec qPow (arg:float) (exp:int) =
+
+    // This operation is not defined.
+    if arg = 0.0 && exp = 0 then
+        failwith "undefined"
+
+    let result =
+
+        // Recursion base case.
+        if exp = 0 then
+            1.0
+        elif exp = 1 then
+            arg
+        else
+            // Divide the exponent by half (floor is taken for an odd argument).
+            let halve = qPow arg (abs exp / 2)
+            // To get an even exponent multiply its halves.
+            if exp % 2 = 0 then
+                halve * halve
+            else
+                // For an odd exponent additionally multiply by the arg.
+                halve * halve * arg
+
+    // For a positive exponent return the value as is.
+    // For a negative exponent inverse the number.
+    if exp > 0 then result else 1.0 / result
+
+
+
+// Homework 1 - Task 3.
+// Difference between the min and the max value in an array.
+// The function takes an array of real numbers and returns the difference
+// between the maximum and the minimum values.
+let diff (arr: float array) : float =
+
+    let result = // This will be returned.
+
+        if arr = [||] then
+            failwith "undefined"
+        else
+
+            let mutable mx = arr[0]
+            let mutable mn = arr[0]
+
+            // Look at each element in an array from left to right
+            // and iteratively find min and max values.
+            for i = 1 to arr.Length - 1 do
+
+                // Max on the current iteration.
+                if arr[i] > mx then mx <- arr[i]
+
+                // Min on the current iteration.
+                if arr[i] < mn then mn <- arr[i]
+
+            mx - mn // The desired value
+
+    result
+
+
+
+// Homework 1 - Task 4.
+// This function returns the array of all odd integers
+// strictly in between two given integers.
+let allOdds (num1: int) (num2: int) : int array =
+
+    // Determine the range
+    let smallerNum: int = if num1 <= num2 then num1 else num2
+
+    let biggerNum: int = if num1 <= num2 then num2 else num1
+
+    // Make an array of all odd integers in the specified range
+    let result: int array =
+        [| for i in (smallerNum + 1) .. (biggerNum - 1) do
+               if abs i % 2 = 1 then yield i |]
+
+    result // Return the array.
---
+++ tests/spbu-fsharp.Tests/Task1Tests.fs
@@ -0,0 +1,101 @@
+module Task1Tests
+
+open Expecto.Flip
+open HomeWork1
+open Expecto
+open FsCheck
+open ExpectoFsCheck
+open ExpectoConfig
+open Microsoft.FSharp.Core
+
+module TestCases =
+
+    let config = { FsCheckConfig.defaultConfig with maxTest = 10000 }
+
+    [<Tests>]
+    let tests =
+
+        testList "samples" [
+
+            testPropertyWithConfig config "pow: All power functions should return equal results"
+                <| fun (x: int) (y: int) ->
+                    if x <> 0 && y <> 0 then
+                        qPow x y = pown x y
+                    else
+                        skiptest "incorrect arguments"
+
+
+            testProperty "pow: Bigger exponent should result in a greater result"
+                <| fun arg exp1 exp2 ->
+                    if exp1 > exp2 && arg > 1 && exp1 > 1 && exp2 > 1 then
+                        qPow arg exp1 > qPow arg exp2
+                    else
+                        skiptest "incorrect arguments"
+
+            testCase "pow: 0 to the 0th power is not defined"
+                <| fun _ ->
+                let actualResult =
+                    Expect.throws (fun _ -> pow 0 0 |> ignore) "undefined"
+                actualResult
+
+            testProperty "qPow: All power functions should return equal results"
+                <| fun (x: int) (y: int) ->
+                    if x <> 0 && y <> 0 then
+                        qPow x y = pown x y
+                    else
+                        skiptest "incorrect arguments"
+
+            testProperty "qPow: Bigger exponent should result in a greater result"
+                <| fun arg exp1 exp2 ->
+                    if exp1 > exp2 && arg > 1 && exp1 > 1 && exp2 > 1 then
+                        qPow arg exp1 > qPow arg exp2
+                    else
+                        skiptest "incorrect arguments"
+
+            testCase "qPow: 0 to the 0th power is not defined"
+                <| fun _ ->
+                    let actualResult = Expect.throws (fun _ -> qPow 0 0 |> ignore) "undefined"
+                    actualResult
+
+            testCase "diff: All numbers in the array are negative."
+                <| fun _ ->
+                    let actualResult = diff [| -1; -2; -3; -4; -5 |]
+                    Expect.equal actualResult 4 "Results don't match"
+
+            testCase "diff: All numbers in the array are positive."
+                <| fun _ ->
+                    let actualResult = diff [| 1; 2; 3; 4; 5 |]
+                    Expect.equal actualResult 4 "Results don't match"
+
+            testCase "diff: An array only has one element"
+                <| fun _ ->
+                    let actualResult = diff [| 5 |]
+                    Expect.equal actualResult 0 "Results don't match"
+
+            testCase "An empty array is given"
+                <| fun _ ->
+                let actualResult = Expect.throws (fun _ -> diff [||] |> ignore) "undefined"
+                actualResult
+
+            testCase "allOdds: In between two positive integers"
+                <| fun _ ->
+                let actualResult = allOdds 1 10
+                Expect.equal actualResult [| 3; 5; 7; 9 |] "Results don't match"
+
+            testProperty "allOdds: assuming ascending order of the resulting array, the first element is determined by input"
+                <| fun a b ->
+                    let smallerNum = min a b
+                    let actualResult = allOdds a b
+                    if Array.length <| actualResult <> 0 then
+                        if abs a % 2 = 1 then
+                            Expect.equal (smallerNum + 2) actualResult[0]
+                        else
+                            Expect.equal (smallerNum + 1) actualResult[0]
+                    else
+                        skiptest "incorrect array"
+
+            testCase "allOdds: The same number is given twice"
+                <| fun _ ->
+                let actualResult = allOdds 5 5
+                Expect.equal actualResult [||] "Results don't match"
+]
