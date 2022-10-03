59425ec95fbf35d98d4df78de024991f118b6dff
artem-burashnikov <artem.burashnikov@gmail.com>
2022-10-03 17:27:43 +0300

    Task2 MyList functions and Tests

---
+++ src/spbu-fsharp/Task2.fs
@@ -0,0 +1,120 @@
+
+module HomeWork2
+
+open Lists
+
+
+
+// Homework 2 - Task 3 - Concat (MyList)
+/// Concatenates two lists.
+// Traverse the first list until Empty.
+// Place the second list at Empty.
+let rec concat (lst1: MyList<'value>) (lst2: MyList<'value>) : MyList<'value> =
+    match lst1 with
+    | Cons (head, tail) -> Cons(head, concat tail lst2)
+    | Empty -> lst2
+
+
+
+// Homework 2 - Task 1 - Bubble sort (MyList).
+/// Sort a linked list using a bubble sorting algorithm.
+// First we calculate the number n of elements in a given list.
+// Then we recursively call the sorting function n times and
+// on each call some element ends up moving to its place .
+let bubbleSort (lst: MyList<'value>) : MyList<'value> =
+
+    // Compare two consecutive values and swap them if required.
+    // On each call this function puts a single element
+    // on its spot in a sorted list.
+    let rec sort (lst: MyList<'value>) : MyList<'value> =
+        match lst with
+        | Empty -> Empty
+        // A single element is already sorted.
+        | Cons (head, Empty) -> Cons(head, Empty)
+        // Swap two consecutive values accordingly.
+        | Cons (head1, Cons (head2, tail)) ->
+            if head1 >= head2 then
+                Cons(head2, sort (Cons(head1, tail)))
+            else
+                Cons(head1, sort (Cons(head2, tail)))
+
+    // Call a sorting function n times,
+    // where n = counter is the length of a given linked list.
+    // This way all elements end up on their spots in the final sorted list.
+    let rec looper lst counter =
+        match counter with
+        | 0 -> lst
+        | counter -> looper (sort lst) (counter - 1)
+
+    // Return a sorted list.
+    looper lst (getLength lst)
+
+
+
+// Homework 2 - Task 2 - Quicksort (MyList)
+/// Sort a linked list using a quicksort algorithm.
+// The first element in a list is chosen as a pivot.
+// First we partition a list into two.
+// The first partition has all elements that are smaller or equal than the pivot.
+// The second partition has all elements that are bigger than the pivot.
+// We then concatenate all three parts in an appropriate order
+// while recursively sorting the partitions the same way.
+let qSort (lst: MyList<'value>) : MyList<'value> =
+    match lst with
+    // No need to sort an empty list or a list that has a single element.
+    // Return immediately.
+    | Empty -> Empty
+    | Cons (head, Empty) -> Cons(head, Empty)
+    | _ ->
+
+        // This sub-function returns all elements
+        // that are less than or equal to the pivot.
+        let rec smallerElements (lst: MyList<'value>) (pivot: 'value) : MyList<'value> =
+            match lst with
+            // If an element is less than or equal to the pivot, then accumulate it.
+            // Otherwise ignore.
+            | Empty -> Empty
+            | Cons (head, Empty) ->
+                if head <= pivot then
+                    Cons(head, Empty)
+                else
+                    Empty
+            | Cons (head, tail) ->
+                if head <= pivot then
+                    Cons(head, smallerElements tail pivot)
+                else
+                    smallerElements tail pivot
+
+        // This function returns all elements that
+        // are greater than the pivot.
+        let rec biggerElements (lst: MyList<'value>) (pivot: 'value) : MyList<'value> =
+            match lst with
+            // If an element is greater than the pivot, then accumulate it.
+            // Otherwise ignore.
+            | Empty -> Empty
+            | Cons (head, Empty) ->
+                if head > pivot then
+                    Cons(head, Empty)
+                else
+                    Empty
+            | Cons (head, tail) ->
+                if head > pivot then
+                    Cons(head, biggerElements tail pivot)
+                else
+                    biggerElements tail pivot
+
+        // This is the main sorting function.
+        // It sorts two partitions and concatenates them with the pivot.
+        let rec sort (lst: MyList<'value>) : MyList<'value> =
+            match lst with
+            // An empty list or a list that has a single element is already sorted.
+            // Otherwise we concatenate and make a recursive call on partitions.
+            | Empty -> Empty
+            | Cons (head, Empty) -> Cons(head, Empty)
+            | Cons (head, tail) ->
+                concat
+                    (sort (smallerElements tail head))
+                    (concat (Cons(head, Empty)) (sort (biggerElements tail head)))
+
+        // Return a sorted list.
+        sort lst
---
+++ tests/spbu-fsharp.Tests/Task2Tests.fs
@@ -0,0 +1,50 @@
+module Task2Tests
+
+open Lists
+open HomeWork2
+open Expecto
+open FsCheck
+open Microsoft.FSharp.Core
+
+
+
+module TestCases =
+
+    let config = { Config.Default with MaxTest = 10000 }
+
+    type MyComparableValues =
+        | Ints of int
+        | Floats of float
+        | Strings of string
+        | Chars of char
+
+    [<Tests>]
+    let tests =
+
+        testList "samples" [
+
+            // I will have to make a FsCheck generator for this custom type.
+            // Some of the comparable types
+            testProperty "Sorting algorithms should produce the same result"
+                <| fun (myList:MyList<MyComparableValues>) ->
+                    let sort1 = bubbleSort myList
+                    let sort2 = qSort myList
+                    Expect.equal (checkEqual sort1 sort2) true
+
+            testCase "If both Empty, result should be Empty"
+                <| fun _ ->
+                    let actualResult = concat Empty Empty
+                    Expect.equal actualResult Empty "Should be empty"
+
+            testProperty "Resulting length should be the sum of initial lengths"
+                <| fun (myList1:MyList<_>) (myList2:MyList<_>) ->
+                    let lengthOfCat = getLength (concat myList1 myList2)
+                    let sumOfLengths = (getLength myList1) + (getLength myList2)
+                    Expect.equal lengthOfCat sumOfLengths "Lengths must match"
+
+            testCase "Empty + something should be something"
+                <| fun _ ->
+                    let actualResult = concat Empty (Cons(2, Cons(2, Empty)))
+                    Expect.equal actualResult (Cons(2, Cons(2, Empty))) "failed to match"
+
+]
