b5027511ce90570d8049d66bec8f7e97db888202
artem-burashnikov <artem.burashnikov@gmail.com>
2022-10-20 20:25:26 +0300

    added more tests

--- tests/spbu-fsharp.Tests/Task3Tests.fs
+++ tests/spbu-fsharp.Tests/Task3Tests.fs
@@ -20,7 +20,7 @@ module TestCases =
                     let input = Node("abba", Empty)
                     let expectedResult = Cons("abba", Empty), 1
                     let actualResult = makeCList input
-                    Expect.equal expectedResult actualResult "Failed to produce a correct list \
+                    Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a single source node."
 
             testCase "A single source Leaf with no children"
@@ -28,7 +28,7 @@ module TestCases =
                     let input = Node(true, Empty)
                     let expectedResult = Cons(true, Empty), 1
                     let actualResult = makeCList input
-                    Expect.equal expectedResult actualResult "Failed to produce a correct list \
+                    Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a single source node."
 
             testCase "A source Node which has a single child (values of both match)"
@@ -36,7 +36,7 @@ module TestCases =
                     let input = Node("abba", Cons(Leaf("abba"), Empty))
                     let expectedResult = Cons("abba", Cons("abba", Empty)), 1
                     let actualResult = makeCList input
-                    Expect.equal expectedResult actualResult "Failed to produce a correct list \
+                    Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a source node with a single child (values of both match)."
 
             testCase "A source Node which has a single child (values of both differ)"
@@ -44,7 +44,7 @@ module TestCases =
                     let input = Node(1, Cons(Leaf(2), Empty))
                     let expectedResult = Cons(1, Cons(2, Empty)), 2
                     let actualResult = makeCList input
-                    Expect.equal expectedResult actualResult "Failed to produce a correct list \
+                    Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a source node with a single child (values of both differ)."
 
             testCase "A source node with two children and a child's child (all values differ)"
@@ -52,7 +52,7 @@ module TestCases =
                     let input = Node(1, Cons(Node(2, Cons(Leaf(3), Empty)), Cons(Leaf(4), Empty)))
                     let expectedResult = Cons(1, Cons(2, Cons(3, Cons(4, Empty)))), 4
                     let actualResult = makeCList input
-                    Expect.equal expectedResult actualResult "Failed to produce a correct list \
+                    Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a source node with two children (all values differ)"
 
             testCase "A source node with two children and a child's child (all values match)"
@@ -60,8 +60,18 @@ module TestCases =
                     let input = Node("abba", Cons(Node("abba", Cons(Leaf("abba"), Empty)), Cons(Leaf("abba"), Empty)))
                     let expectedResult = Cons("abba", Cons("abba", Cons("abba", Cons("abba", Empty)))), 1
                     let actualResult = makeCList input
-                    Expect.equal expectedResult actualResult "Failed to produce a correct list \
+                    Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a source node with two children and a child's child (all values match)"
+
+            testCase "A single source node that contains an empty list as a value"
+                <| fun _ ->
+                    let input1 = Node(Empty, Empty)
+                    let input2 = Leaf(Empty)
+                    let expectedResult = Cons(Empty, Empty), 1
+                    let actualResult1 = makeCList input1
+                    let actualResult2 = makeCList input2
+                    Expect.equal (actualResult1 = expectedResult && actualResult2 = expectedResult) true "Failed to produce \
+                        a correct list that has an empty list as an element."
 
             testProperty "Number of unique elements in a tree must be no more than number of all elements."
                <| fun tree ->
@@ -69,4 +79,8 @@ module TestCases =
                     let length = getLength (fst lst)
                     snd lst <= length
 
+            testProperty "A constructed list cannot be empty"
+                <| fun tree ->
+                    let lst = makeCList tree
+                    getLength (fst lst) > 0 && snd lst >= 0
 ]
