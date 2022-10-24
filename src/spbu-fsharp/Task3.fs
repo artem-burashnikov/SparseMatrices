69a115f5d34f53bd45f17bc1992d17c68a128f4d
artem-burashnikov <artem.burashnikov@gmail.com>
2022-10-24 12:05:34 +0300

    refactoring according to the specification

--- src/spbu-fsharp/Task3.fs
+++ src/spbu-fsharp/Task3.fs
@@ -16,10 +16,10 @@ module NTrees =
 
 
     /// General n-ary tree folding function.
-    let rec fold folder acc item =
+    let rec fold folder acc tree =
         let recurse = fold folder
 
-        match item with
+        match tree with
         | Leaf value -> folder acc value
         | Node (value, children) -> CLists.fold recurse (folder acc value) children
 
@@ -27,27 +27,23 @@ module NTrees =
 
     /// Function makes a set of values in the nodes of an n-ary tree.
     // We pass hSetAdd as folder function to the general tree folding function.
-    // Returns a set of elements.
-    let setFromValues tree =
+    // Returns a count of unique elements.
+    let countUniques tree =
         let hSetAdd (hSet: HashSet<'Value>) value =
             hSet.Add value |> ignore
             hSet
 
         let hSet = HashSet<'Value>()
-        fold hSetAdd hSet tree
+        let result = fold hSetAdd hSet tree
+        result.Count
 
 
 
     /// Function constructs a linked list of type CList from the values in the nodes of n-ary tree.
-    // lstAdd is passed as folder function to the general tree folding function.
+    // lstAdd is passed as a folder function to the general tree folding function.
     // Returns a CList that has all values.
-    let cListConstruct tree =
-        // The defined order of arguments (1st: elem; 2nd: cList) is important.
-        // When this gets passed as a folder function, the accumulator will be the first argument.
+    let mkCList tree =
+        // The defined order of arguments (1st: cList; 2nd: elem) is important.
+        // When this gets passed to fold as a folder function, the accumulator will be the first argument.
         let lstAdd cList elem = Cons(elem, cList)
         fold lstAdd Empty tree
-
-
-    // HomeWork 3 result..
-    let traverse tree =
-        cListConstruct tree, (setFromValues tree).Count
--- tests/spbu-fsharp.Tests/Task3Tests.fs
+++ tests/spbu-fsharp.Tests/Task3Tests.fs
@@ -20,7 +20,7 @@ module TestCases =
                 <| fun _ ->
                     let input = Node("abba", Empty)
                     let expectedResult = Cons("abba", Empty), 1
-                    let actualResult = traverse input
+                    let actualResult = mkCList input, countUniques input
                     Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a single source node."
 
@@ -28,7 +28,7 @@ module TestCases =
                 <| fun _ ->
                     let input = Leaf("abba")
                     let expectedResult = Cons("abba", Empty), 1
-                    let actualResult = traverse input
+                    let actualResult = mkCList input, countUniques input
                     Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a single source node."
 
@@ -36,7 +36,7 @@ module TestCases =
                 <| fun _ ->
                     let input = Node("abba", Cons(Leaf("abba"), Empty))
                     let expectedResult = Cons("abba", Cons("abba", Empty)), 1
-                    let actualResult = traverse input
+                    let actualResult = mkCList input, countUniques input
                     Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a source node with a single child (values of both match)."
 
@@ -44,7 +44,7 @@ module TestCases =
                 <| fun _ ->
                     let input = Node(1, Cons(Leaf(2), Empty))
                     let expectedResult = Cons(2, Cons(1, Empty)), 2
-                    let actualResult = traverse input
+                    let actualResult = mkCList input, countUniques input
                     Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a source node with a single child (values of both differ)."
 
@@ -52,7 +52,7 @@ module TestCases =
                 <| fun _ ->
                     let input = Node(1, Cons(Node(2, Cons(Leaf(3), Empty)), Cons(Leaf(4), Empty)))
                     let expectedResult = Cons(4, Cons(3, Cons(2, Cons(1, Empty)))), 4
-                    let actualResult = traverse input
+                    let actualResult = mkCList input, countUniques input
                     Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a source node with two children (all values differ)"
 
@@ -60,7 +60,7 @@ module TestCases =
                 <| fun _ ->
                     let input = Node("abba", Cons(Node("abba", Cons(Leaf("abba"), Empty)), Cons(Leaf("abba"), Empty)))
                     let expectedResult = Cons("abba", Cons("abba", Cons("abba", Cons("abba", Empty)))), 1
-                    let actualResult = traverse input
+                    let actualResult = mkCList input, countUniques input
                     Expect.equal actualResult expectedResult "Failed to produce a correct list \
                         on a source node with two children and a child's child (all values match)"
 
@@ -69,20 +69,20 @@ module TestCases =
                     let input1 = Node(Empty, Empty)
                     let input2 = Leaf(Empty)
                     let expectedResult = Cons(Empty, Empty), 1
-                    let actualResult1 = traverse input1
-                    let actualResult2 = traverse input2
+                    let actualResult1 = mkCList input1, countUniques input1
+                    let actualResult2 = mkCList input2, countUniques input2
                     Expect.equal (actualResult1 = expectedResult && actualResult2 = expectedResult) true "Failed to produce \
                         a correct list that has an empty list as an element."
 
             testProperty "Number of unique elements in a tree must be no more than number of all elements."
                <| fun tree ->
-                    let lst = traverse tree
+                    let lst = mkCList tree, countUniques tree
                     let length = getLength (fst lst)
                     snd lst <= length
 
             testProperty "A constructed list cannot be empty"
                 <| fun tree ->
-                    let result = traverse tree
+                    let result = mkCList tree, countUniques tree
                     getLength (fst result) > 0 && snd result >= 0
 
             testProperty "Constructing a list from a given tree then making a set of values \
@@ -99,10 +99,19 @@ module TestCases =
 
                         CLists.fold hSetAdd hSet lst
 
+                    // Defines a folder function for NTree.fold
+                    let mkHashSet tree =
+                        let hSetAdd (hSet: HashSet<'Value>) value =
+                            hSet.Add value |> ignore
+                            hSet
+
+                        let hSet = HashSet<'Value>()
+                        fold hSetAdd hSet tree
+
                     // We fold a given tree into a set of its elements (set1) and a CList.
                     // After which we fold a CList into the set of its elements (set2).
                     // Both sets (set1 and set2) should be subsets of each other.
-                    let resultFromTree = cListConstruct tree, setFromValues tree
+                    let resultFromTree = mkCList tree, mkHashSet tree
                     let setFromTree = snd resultFromTree
                     let setFromCList = lstUniques <| fst resultFromTree
 
