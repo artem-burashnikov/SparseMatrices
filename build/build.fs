54e8fec343b39c517adb5024a82242e477d9c183
artem-burashnikov <artem.burashnikov@gmail.com>
2022-12-19 12:21:14 +0300

    linter on build.fs

--- build/build.fs
+++ build/build.fs
@@ -222,9 +222,9 @@ module dotnet =
 module FSharpAnalyzers =
     type Arguments =
     | Project of string
-    | Analyzers_Path of string
-    | Fail_On_Warnings of string list
-    | Ignore_Files of string list
+    | AnalyzersPath of string
+    | FailOnWarnings of string list
+    | IgnoreFiles of string list
     | Verbose
     with
         interface IArgParserTemplate with
@@ -360,9 +360,9 @@ let fsharpAnalyzers _ =
     |> Seq.iter(fun proj ->
         let args  =
             [
-                FSharpAnalyzers.Analyzers_Path (__SOURCE_DIRECTORY__ </> ".." </> "packages/analyzers")
+                FSharpAnalyzers.AnalyzersPath (__SOURCE_DIRECTORY__ </> ".." </> "packages/analyzers")
                 FSharpAnalyzers.Arguments.Project proj
-                FSharpAnalyzers.Arguments.Fail_On_Warnings [
+                FSharpAnalyzers.Arguments.FailOnWarnings [
                     "BDH0002"
                 ]
                 FSharpAnalyzers.Verbose
