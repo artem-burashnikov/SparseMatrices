a54b8618b2b0351a10fc9c1b6cb280d09166f76b
artem <artem.burashnikov@gmail.com>
2023-02-02 17:50:09 +0300

    Change graph constructor

--- src/spbu-fsharp/Graphs.fs
+++ src/spbu-fsharp/Graphs.fs
@@ -1,17 +1,41 @@
 module Graphs
 
 open SparseMatrix.SparseMatrix
+open Helpers.Numbers
 
 type Vertex<'A> = 'A
 
 type Edge<'A> = 'A * 'A
 
-type Graph<'A, 'B when 'A: comparison and 'B: equality> =
-    val Vertices: Set<Vertex<'A>>
-    val Edges: Set<Edge<'A>>
-    val AdjMtx: SparseMatrix<'B>
 
-    new(vertices, edges, adjMtx) =
-        { Vertices = vertices
-          Edges = edges
-          AdjMtx = adjMtx }
+type Graph<'A when 'A: equality>(adjMtx: SparseMatrix<'A>) =
+
+    let verticesOfMtx (mtx: SparseMatrix<'A>) : Set<Vertex<uint>> =
+        Set.ofSeq (
+            seq {
+                for i = 1 to (toIntConv mtx.Rows) do
+                    yield uint i
+            }
+        )
+
+    let edgesOfMtx (mtx: SparseMatrix<'A>) : Set<Edge<uint>> =
+        Set.ofSeq (
+            seq {
+                for i = 1 to toIntConv mtx.Rows do
+                    for j = 1 to toIntConv mtx.Columns do
+                        let value = mtx[uint i, uint j]
+
+                        if value <> Option.None then
+                            yield (uint i - 1u, uint j - 1u)
+            }
+        )
+
+    let vertices = verticesOfMtx adjMtx
+
+    let edges = edgesOfMtx adjMtx
+
+    member this.Vertices = vertices
+
+    member this.Edges = edges
+
+    member this.AdjMtx = adjMtx
