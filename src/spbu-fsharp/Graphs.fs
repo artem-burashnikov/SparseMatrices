97aba11aa4faa007294a8f0791595e9d6ec87d48
artem <artem.burashnikov@gmail.com>
2023-02-20 15:25:52 +0300

    Change edgesOfMtx in Graphs to be a fold static method in SparseMatrix

--- src/spbu-fsharp/Graphs.fs
+++ src/spbu-fsharp/Graphs.fs
@@ -2,7 +2,6 @@ module Graphs
 
 open SparseMatrix.SparseMatrix
 open Helpers.Numbers
-open Trees.QuadTrees
 
 type Graph<'A when 'A: equality>(adjMtx: SparseMatrix<'A>) =
 
@@ -16,20 +15,8 @@ type Graph<'A when 'A: equality>(adjMtx: SparseMatrix<'A>) =
 
     let edgesOfMtx (mtx: SparseMatrix<'A>) =
 
-        let rec inner collector tree result i j size =
-            match tree with
-            | QuadTree.None -> result
-            | QuadTree.Leaf _ -> collector i j result
-            | QuadTree.Node(nw, ne, sw, se) ->
-                let half = size / 2u
-                let resultFromNW = inner collector nw result i j half
-                let resultFromNE = inner collector ne resultFromNW i (j + half) half
-                let resultFromSW = inner collector sw resultFromNE (i + half) j half
-                inner collector se resultFromSW (i + half) (j + half) half
-
-        let collector i j set = Set.add (Set.ofList [ i; j ]) set
-        let size = ceilPowTwo (max mtx.Rows mtx.Columns)
-        inner collector mtx.Data Set.empty 0u 0u size
+        let folder i j set = Set.add (Set.ofList [ i; j ]) set
+        SparseMatrix.Fold folder Set.empty mtx
 
     member this.Vertices = verticesOfMtx adjMtx
 
--- src/spbu-fsharp/SparseMatrix.fs
+++ src/spbu-fsharp/SparseMatrix.fs
@@ -1,5 +1,8 @@
 namespace SparseMatrix
 
+open Microsoft.Diagnostics.Tracing.Parsers.Clr
+open Microsoft.Diagnostics.Tracing.Parsers.Kernel
+open Trees
 open Trees.QuadTrees
 open Helpers
 
@@ -227,3 +230,19 @@ module SparseMatrix =
                 getValue i j
 
         member this.IsEmpty = this.Data = QuadTree.None
+
+        static member Fold folder state (mtx: SparseMatrix<'A>) =
+
+            let rec inner folder i j result size qTree =
+                match qTree with
+                | QuadTree.None -> result
+                | QuadTree.Leaf _ -> folder i j result
+                | QuadTree.Node(nw, ne, sw, se) ->
+                    let half = size / 2u
+                    let resultFromNW = inner folder i j result half nw
+                    let resultFromNE = inner folder i (j + half) resultFromNW half ne
+                    let resultFromSW = inner folder (i + half) j resultFromNE half sw
+                    inner folder (i + half) (j + half) resultFromSW half se
+
+            let size = Numbers.ceilPowTwo (max mtx.Rows mtx.Columns)
+            inner folder 0u 0u state size mtx.Data
