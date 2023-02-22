49d196c908845fbd70e8386cfdc01085e012bccc
artem <artem.burashnikov@gmail.com>
2023-02-22 14:32:53 +0300

    Add Vertex and Edge types

--- src/spbu-fsharp/Graphs.fs
+++ src/spbu-fsharp/Graphs.fs
@@ -3,19 +3,25 @@ module Graphs
 open SparseMatrix.SparseMatrix
 open Helpers.Numbers
 
+type Vertex = Vertex of uint
+
+type Edge<'Label> = Edge of Vertex * Vertex * 'Label
+
 type Graph<'A when 'A: equality and 'A: comparison>(adjMtx: SparseMatrix<'A>) =
 
     let verticesOfMtx (mtx: SparseMatrix<'A>) =
         Set.ofSeq (
             seq {
                 for i = 1 to (toIntConv mtx.Rows) do
-                    yield uint i
+                    yield Vertex(uint i)
             }
         )
 
     let edgesOfMtx (mtx: SparseMatrix<'A>) =
 
-        let folder i j value set = Set.add (i, j, value) set
+        let folder i j value set =
+            Set.add (Edge(Vertex i, Vertex j, value)) set
+
         SparseMatrix.Fold folder Set.empty mtx
 
     member this.Vertices = verticesOfMtx adjMtx
